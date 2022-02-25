677a699c9ec85790b211fccfdc5e815aea5ff880
Busyairi AR <busyairiar@gmail.com>
2022-02-25 07:11:44 +0700

    setup deploy heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const router = require('./routes/index')
 const session = require('express-session')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
